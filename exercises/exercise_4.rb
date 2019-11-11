c144fc7058c1379f5f6b1ccd1d0cbbdfd3117c62
Alan Dodge <alphaechododge@gmail.com>
2019-11-11 22:08:49 +0000

    Completed up to exercise 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 19000000, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |mens_store|
+  puts "Store: #{mens_store.name} Revenue: #{mens_store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womens_stores.each do |womens_store|
+  puts "Store: #{womens_store.name} Revenue: #{womens_store.annual_revenue}"
+end
